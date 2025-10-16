f5899b33f68f431808fb88aa7454f57a5ed0a2b3
govgranter <luckyiphonegrab@gmail.com>
2025-10-16 20:22:01 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000; 
