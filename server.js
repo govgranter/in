426e1730321eb79c825eeb7d4a5bf7065dd3b383
govgranter <luckyiphonegrab@gmail.com>
2025-11-29 11:20:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/api/messages', (req, res) => {
     }
 
     const newMessage = {
-        User: empty.trim(),
+        UserID: empty.trim(),
         id: Date.now().toString(),
         text: text.trim(),
         timestamp: new Date().toISOString()
