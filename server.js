4462860a895e5ea1fcec48ded6c334a046fa8193
govgranter <luckyiphonegrab@gmail.com>
2025-11-29 10:16:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/api/messages', (req, res) => {
 
 // Get all messages (for initial load)
 app.get('/api/messages/all', (req, res) => {
-    res.json(messages);
+    res.json(clients);
 });
 
 // Clear all messages (optional endpoint)
